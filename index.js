a3eb0d678ba6af06bfe64e7c2ec93baef649ad60
mayvid14 <ndivyam@gmail.com>
2017-06-28 23:57:40 +0545

    encryption added

--- index.js
+++ index.js
@@ -7,13 +7,30 @@ var urlenc = bodyParser.urlencoded({
     extended: false
 });
 var jsonenc = bodyParser.json();
+var bcrypt = require('bcrypt');
+const saltRounds = 10;
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/nm', express.static(path.join(__dirname, 'node_modules')));
 app.get('/', jsonenc, function (req, res) {
     res.sendFile(path.join(__dirname, 'public', 'views', 'index.html'));
 });
 app.get('/user', urlenc, function (req, res) {
-    res.send('Welcome ' + req.query.user);
+    var shash = "";
+    bcrypt.hash('password', saltRounds, function (err, hash) {
+        shash = hash;
+        bcrypt.compare(req.query.pwd, shash, function (err, resp) {
+            if (resp) {
+                res.send({
+                    'message': 'Successful'
+                });
+            }
+            else {
+                res.send({
+                    'message': 'Not successful'
+                });
+            }
+        });
+    });
 });
 app.listen(8080, function () {
     console.log('Connected to 8080');
